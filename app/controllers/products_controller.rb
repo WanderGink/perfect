33c71d6d314325e82bfb5486ffd417b32abbebbc
WanderGink <ginksad@gmail.com>
2017-05-26 14:45:56 +0700

    CRUD product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,62 @@
+class ProductsController < ApplicationController
+  before_action :find_product, except: [:index, :new, :create]
+
+  def index
+    @products = Product.all
+    @categories = Category.all
+  end
+
+  def new
+    @product = current_user.products.build
+    @categories = Category.all
+  end
+
+  def create
+    @product = current_user.products.build product_params
+    respond_to do |format|
+      if @product.save
+        format.html{redirect_to products_url}
+      else
+        format.html{render :new}
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    @categories = Category.all
+  end
+
+  def update
+    respond_to do |format|
+      if @product.update product_params
+        format.html{redirect_to products_url}
+      else
+        format.html{render :edit}
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html{redirect_to products_url}
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit :id, :title, :price, :description,
+      :avg_rating, :category_id, :product_image, :user_id
+  end
+
+  def find_product
+    @product = Product.find_by id: params[:id]
+    unless @product
+      flash[:error] = t("product_not_found")
+      redirect_to root_url
+    end
+  end
+end
